088bee025986b60c870b801bdaa7654447b65d8a
mpirixan <mpirixan@ufpa.br>
2021-05-12 21:32:19 -0300

    First commit

---
+++ src/learning/Learning.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learning;
+
+import javax.lang.model.type.NullType;
+import java.lang.Math;
+/**
+ *
+ * @author mpirixan
+ */
+public class Learning {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    char numbers[] = {'a','a','a','a','a'};
+    int max = numbers.length;
+    int index = max/max;
+    max *= max;
+    for (int i = index; index <= max; index++ ){
+    System.out.println(index);
+    }
+}
+    
+}
