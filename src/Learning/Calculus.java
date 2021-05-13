ed0250580b535bcf1a0933a26cf68835743e0f4e
mpirixan <mpirixan@ufpa.br>
2021-05-13 13:24:04 -0300

    Teste de soma

---
+++ src/Learning/Calculus.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Learning;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author mpirixan
+ */
+public class Calculus {
+    int sum = 0;
+    public int addTwoInt(int a, int b) {
+        sum = a + b;
+          
+        //returning summation of two values.
+    return sum; 
+
+    }
+    
+}
--- src/Learning/Main.java
+++ src/Learning/Main.java
@@ -3,27 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package learning;
-
+package Learning;
 import javax.lang.model.type.NullType;
 import java.lang.Math;
+import java.util.Arrays;
 /**
  *
  * @author mpirixan
  */
-public class Learning {
-
+public class Main {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    char numbers[] = {'a','a','a','a','a'};
-    int max = numbers.length;
-    int index = max/max;
-    max *= max;
-    for (int i = index; index <= max; index++ ){
-    System.out.println(index);
-    }
-}
+
+    Calculus add = new Calculus();
+    int s = add.addTwoInt(5,5);
+    System.out.println("Sum of two integer values :"+ s);
     
+    }  
 }
+
